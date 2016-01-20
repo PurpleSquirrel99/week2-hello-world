30ea5556e1648177d4d4da697c4938670a513438
John Brown <jbrown312@mail.valenciacollege.edu>
2016-01-20 04:47:48 -0800

    I finished week  2

---
+++ Lan.py
@@ -0,0 +1,9 @@
+print('Hello, please enter the language you want me to greet you in.')
+print('Zulu,Japanese,Arabic')    # ask for their name
+LName1 = input()
+if LName1 == 'Japanese':#
+    print('Kon nichiwa')#
+if LName1 == 'Zulu':    ####
+    print('Sawubona')   ####Replying
+if LName1 == 'Arabic':  #
+    print('Murhaba')    #
