8ff72052f63f0cad8031562b43c9912e64227d0a
John Brown <jbrown312@mail.valenciacollege.edu>
2016-01-27 23:14:43 -0800

    fixed the number part

--- Lan.py
+++ Lan.py
@@ -1,9 +1,9 @@
 print('Hello, please enter the language you want me to greet you in.')
-print('Zulu,Japanese,Arabic')    # ask for their name
+print('(1)Zulu,(2)Japanese,(3)Arabic')    # ask for their name
 LName1 = input()
-if LName1 == 'Japanese':#
+if LName1 == '2':       #
     print('Kon nichiwa')#
-if LName1 == 'Zulu':    ####
+if LName1 == '1':       ####
     print('Sawubona')   ####Replying
-if LName1 == 'Arabic':  #
+if LName1 == '3':       #
     print('Murhaba')    #
